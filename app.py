47eabb8a3b31338ac565c7b9d7f2e9c5aabe2201
pandyandy <144467422+pandyandy@users.noreply.github.com>
2024-07-02 21:40:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
+st.subheader('Hello, world!')
 st.write('Hello, world!')
+
+if st.button('Say hello'):
+    st.write('Hello, my friend!')
+
+# create a dummy app
+data = {'name': 'John', 'age': 25, 'city': 'New York'}
+st.dataframe(data)
